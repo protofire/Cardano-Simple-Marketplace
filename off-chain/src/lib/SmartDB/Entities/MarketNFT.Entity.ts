42cef2e93dfdec44973fe81d84e0a28a8b431431
manupadillaph <manuepad@gmail.com>
2024-11-12 03:12:19 -0300

    add _is_NET_id_Unique and _NET_id_TN and bigint instead of BigInt

--- off-chain/src/lib/SmartDB/Entities/MarketNFT.Entity.ts
+++ off-chain/src/lib/SmartDB/Entities/MarketNFT.Entity.ts
@@ -2,17 +2,23 @@ import 'reflect-metadata';
 
 import { asSmartDBEntity, BaseSmartDBEntity, Convertible, type CS, type TN } from 'smart-db';
 
-import {type PaymentKeyHash } from 'lucid-cardano';
+import { type PaymentKeyHash } from 'lucid-cardano';
+import { mintingPolicyID_TN } from '../../Commons/Constants/onchain';
 
 @asSmartDBEntity()
 export class MarketNFTEntity extends BaseSmartDBEntity {
     protected static _apiRoute: string = 'marketnft';
     protected static _className: string = 'MarketNFT';
+    protected static _plutusDataIsSubType = false;
 
     // #region fields
 
+    // si tiene id NFT en el datum o si es un FT
+    protected static _is_NET_id_Unique = false;
+    _NET_id_TN: string = mintingPolicyID_TN;
+
     @Convertible({ isForDatum: true })
-    sellerAddress!: PaymentKeyHash;
+    sellerPaymentPKH!: PaymentKeyHash;
 
     @Convertible({ isForDatum: true })
     policyID_CS!: CS;
@@ -24,10 +30,11 @@ export class MarketNFTEntity extends BaseSmartDBEntity {
     sellingToken_TN!: TN;
 
     @Convertible({ isForDatum: true })
-    priceOfAsset!: BigInt;
+    priceOfAsset!: bigint;
 
     @Convertible({ isForDatum: true })
-    minADA!: BigInt;
+    minADA!: bigint;
+
     // #endregion fields
 
     // #region db
@@ -37,7 +44,7 @@ export class MarketNFTEntity extends BaseSmartDBEntity {
     public static alwaysFieldsForSelect: Record<string, boolean> = {
         ...super.alwaysFieldsForSelect,
 
-        sellerAddress: true,
+        sellerPaymentPKH: true,
         policyID_CS: true,
         sellingToken_CS: true,
         sellingToken_TN: true,
